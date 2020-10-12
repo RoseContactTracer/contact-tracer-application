7e69778e3ecbe695327466fe7c82b86751e7a13b
Maura Coriale <corialmt@rose-hulman.edu>
2020-10-11 22:09:25 -0400

    Created initial repo populated using Spring Initializer

---
+++ src/test/java/edu/rosehulman/covidtracer/covidtracerapplication/CovidTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rosehulman.covidtracer.covidtracerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
