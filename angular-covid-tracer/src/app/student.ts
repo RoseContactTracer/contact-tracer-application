fc204a88cb5ab6b37c193436c52e1ee9815b6cee
thomasnb <thomasnb@thomasnb-5520.mshome.net>
2020-12-17 14:08:15 -0500

    displaying our new data from the person and students table

--- angular-covid-tracer/src/app/student.ts
+++ angular-covid-tracer/src/app/student.ts
@@ -1,5 +1,12 @@
 export interface Student {
     id: number;
-    name: string;
+    studentID: number;
+    greekAfiliation: string;
+    fName: string;
+    mName: string;
+    lName: string;
+    email: string;
+    phone: string;
+    residence: string;
     isPositive: boolean;
   }
