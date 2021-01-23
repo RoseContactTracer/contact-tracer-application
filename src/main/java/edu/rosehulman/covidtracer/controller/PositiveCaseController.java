d6136d13364c4a1bb37970ede8acbac463e2a42f
Jake <you@example.com>
2021-01-23 16:56:58 -0500

    Added some more api endpoints.

--- src/main/java/edu/rosehulman/covidtracer/controller/PositiveCaseController.java
+++ src/main/java/edu/rosehulman/covidtracer/controller/PositiveCaseController.java
@@ -1,5 +1,6 @@
 package edu.rosehulman.covidtracer.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,13 +11,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.rosehulman.covidtracer.model.ContactTracer;
 import edu.rosehulman.covidtracer.model.Person;
 import edu.rosehulman.covidtracer.model.PositiveCase;
+import edu.rosehulman.covidtracer.repository.ContactTracerRepository;
 import edu.rosehulman.covidtracer.repository.PersonRepository;
 import edu.rosehulman.covidtracer.repository.PositiveCaseRepository;
 import edu.rosehulman.covidtracer.service.PositiveCaseService;
@@ -33,6 +37,9 @@ public class PositiveCaseController {
 	
 	@Autowired
 	private PersonRepository personRepo;
+	
+	@Autowired
+	private ContactTracerRepository contactTracerRepo;
 	
 	@GetMapping("/positive-case")
     public ResponseEntity<List<PositiveCase>> getAllPositiveCases(@RequestParam(defaultValue = "0") Integer pageNum,
@@ -53,12 +60,36 @@ public class PositiveCaseController {
 	
 	@PostMapping("/people/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public PositiveCase createPositiveCase(@PathVariable (value = "id") Long personID, @RequestBody PositiveCase positiveCase) throws NotFoundException {
+	public PositiveCase createPositiveCaseOnPersonProfile(@PathVariable (value = "id") Long personID, @RequestBody PositiveCase positiveCase) throws NotFoundException {
 		return personRepo.findById(personID).map(person -> {
 			positiveCase.setPerson(person);
 			return repository.save(positiveCase);
 		}).orElseThrow(() -> new NotFoundException("PersonID" + personID + " not found"));
 
 	}
+	
+	@PostMapping("/people")
+	@ResponseStatus(HttpStatus.CREATED)
+	public PositiveCase createPositiveCaseOnPeopleList(@RequestParam (name = "id") Long personID, @RequestBody PositiveCase positiveCase) throws NotFoundException {
+		return personRepo.findById(personID).map(person -> {
+			positiveCase.setPerson(person);
+			return repository.save(positiveCase);
+		}).orElseThrow(() -> new NotFoundException("PersonID" + personID + " not found"));
+
+	}
+	
+	  @PutMapping("/positive-case/{id}")
+	  public ResponseEntity<PositiveCase> updateContactTracer(@PathVariable("id") long id, @RequestParam(name = "contactTracerID") Long contactTracerID) {
+	    Optional<PositiveCase> existingCase = repository.findById(id);
+	    Optional<ContactTracer> tracer = contactTracerRepo.findById(contactTracerID);
+
+	    if (existingCase.isPresent() && tracer.isPresent()) {
+	      PositiveCase _case = existingCase.get();
+	      _case.setContactTracer(tracer.get());
+	      return new ResponseEntity<>(repository.save(_case), HttpStatus.OK);
+	    } else {
+	      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	    }
+	  }
 	
 }
