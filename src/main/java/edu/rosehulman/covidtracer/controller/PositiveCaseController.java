e68cdee0e3472c9196c43c7015a1482e50f6f54b
Jake <you@example.com>
2021-01-29 12:10:19 -0500

    Receiving post now.

--- src/main/java/edu/rosehulman/covidtracer/controller/PositiveCaseController.java
+++ src/main/java/edu/rosehulman/covidtracer/controller/PositiveCaseController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -82,7 +83,8 @@ public class PositiveCaseController {
 		}).orElseThrow(() -> new NotFoundException("PersonID" + personID + " not found"));
 
 	}
-
+	
+	@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 	@PutMapping("/positive-case/{id}")
 	public ResponseEntity<PositiveCase> updateContactTracer(@PathVariable("id") long id,
 			@RequestParam(name = "contactTracerID") Long contactTracerID) {
@@ -99,3 +101,5 @@ public class PositiveCaseController {
 	}
 
 }
+
+
