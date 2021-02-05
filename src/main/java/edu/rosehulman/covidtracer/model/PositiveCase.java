49d98f62e71ada699e59f9a33d62c8d0e8196e28
Noah Thomas <noahthomas88@gmail.com>
2021-02-04 19:03:23 -0500

    changing the names

--- src/main/java/edu/rosehulman/covidtracer/model/PositiveCase.java
+++ src/main/java/edu/rosehulman/covidtracer/model/PositiveCase.java
@@ -41,12 +41,12 @@ public class PositiveCase implements Serializable {
 	public PositiveCase() {
 	}
 
-	public PositiveCase(Long ID, Person person, Date testAdministrationDate, Date startOfSymptoms, Date endOfQuarentineDate, boolean needsTransportation) {
+	public PositiveCase(Long ID, Person person, Date testDate, Date symptomaticStartDate, Date endOfQuarentineDate, boolean needsTransportation) {
 		this.ID = ID;
 		this.person = person;
-		this.testDate = testAdministrationDate;
-		this.symptomaticStartDate = startOfSymptoms;
-		this.quarantineEndDate = endOfQuarentineDate;
+		this.testDate = testDate;
+		this.symptomaticStartDate = symptomaticStartDate;
+		this.quarantineEndDate = quarantineEndDate;
 		this.needsTransportation = needsTransportation;
 	}
 
