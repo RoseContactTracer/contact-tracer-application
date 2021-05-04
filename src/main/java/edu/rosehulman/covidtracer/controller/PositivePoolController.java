c7c12696da0a8d3c6f4f8fe488feb4becb1565f0
hanseljt <hanseljt@hanseljt-5520.rose-hulman.edu>
2021-05-04 13:53:30 -0400

    refactored PositivePoolController.addNewPositivePool

--- src/main/java/edu/rosehulman/covidtracer/controller/PositivePoolController.java
+++ src/main/java/edu/rosehulman/covidtracer/controller/PositivePoolController.java
@@ -48,51 +48,8 @@ public class PositivePoolController {
 	
 	@PostMapping("/positive-pool")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<List<PositivePool>> createPositiveCaseOnPersonProfile(@RequestBody String[] emails) throws NotFoundException {
-		Person person1, person2, person3, person4;
-		PositivePool newPool = new PositivePool();
-		newPool.setDate();
-		poolRepo.save(newPool);
-		
-		if(emails[0] == null && emails[1] == null && emails[2] == null && emails[3] == null) {
-			throw new NotFoundException("You must enter at least one person");
-		}
-		
-		if(emails[0] != null) {
-			person1 = personRepo.findByEmail(emails[0])
-					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
-			newPool.addPerson(person1);
-			person1.addPositivePools(newPool);
-			personRepo.save(person1);
-		}
-		
-		if(emails[1] != null) {
-			person2 = personRepo.findByEmail(emails[1])
-					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
-			newPool.addPerson(person2);
-			person2.addPositivePools(newPool);
-			personRepo.save(person2);
-		}
-		
-		if(emails[2] != null) {
-			person3 = personRepo.findByEmail(emails[2])
-					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
-			newPool.addPerson(person3);
-			person3.addPositivePools(newPool);
-			personRepo.save(person3);
-		}
-		
-		if(emails[3] != null) {
-			person4 = personRepo.findByEmail(emails[3])
-					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
-			newPool.addPerson(person4);
-			person4.addPositivePools(newPool);
-			personRepo.save(person4);
-		}
-		
-		poolRepo.save(newPool);
-		List<PositivePool> resultSet = new ArrayList<PositivePool>();
-		resultSet.add(newPool);
+	public ResponseEntity<List<PositivePool>> addNewPositivePool(@RequestBody String[] emails) throws NotFoundException {
+		List<PositivePool> resultSet = service.addPool(emails);
 		return new ResponseEntity<List<PositivePool>>(resultSet, new HttpHeaders(), HttpStatus.OK);
 
 	}
--- src/main/java/edu/rosehulman/covidtracer/service/PositivePoolService.java
+++ src/main/java/edu/rosehulman/covidtracer/service/PositivePoolService.java
@@ -10,13 +10,18 @@ import org.springframework.stereotype.Service;
 
 import edu.rosehulman.covidtracer.model.Person;
 import edu.rosehulman.covidtracer.model.PositivePool;
+import edu.rosehulman.covidtracer.repository.PersonRepository;
 import edu.rosehulman.covidtracer.repository.PositivePoolRepository;
+import javassist.NotFoundException;
 
 @Service
 public class PositivePoolService {
 
     @Autowired
     PositivePoolRepository repository;
+	
+	@Autowired
+	private PersonRepository personRepo;
 
     @PostConstruct
     private void showRepositoryInfo() {
@@ -29,6 +34,54 @@ public class PositivePoolService {
             return result;
         }
         return new ArrayList<PositivePool>();
+    }
+    
+    public List<PositivePool> addPool(String[] emails) throws NotFoundException {
+    	Person person1, person2, person3, person4;
+		PositivePool newPool = new PositivePool();
+		newPool.setDate();
+		repository.save(newPool);
+		
+		if(emails[0] == null && emails[1] == null && emails[2] == null && emails[3] == null) {
+			throw new NotFoundException("You must enter at least one person");
+		}
+		
+		if(emails[0] != null) {
+			person1 = personRepo.findByEmail(emails[0])
+					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
+			newPool.addPerson(person1);
+			person1.addPositivePools(newPool);
+			personRepo.save(person1);
+		}
+		
+		if(emails[1] != null) {
+			person2 = personRepo.findByEmail(emails[1])
+					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
+			newPool.addPerson(person2);
+			person2.addPositivePools(newPool);
+			personRepo.save(person2);
+		}
+		
+		if(emails[2] != null) {
+			person3 = personRepo.findByEmail(emails[2])
+					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
+			newPool.addPerson(person3);
+			person3.addPositivePools(newPool);
+			personRepo.save(person3);
+		}
+		
+		if(emails[3] != null) {
+			person4 = personRepo.findByEmail(emails[3])
+					.orElseThrow(() -> new NotFoundException("Person Email" + emails[0] + " not found"));
+			newPool.addPerson(person4);
+			person4.addPositivePools(newPool);
+			personRepo.save(person4);
+		}
+		
+		repository.save(newPool);
+		List<PositivePool> resultSet = new ArrayList<PositivePool>();
+		resultSet.add(newPool);
+		return resultSet;
     }
 
 }
